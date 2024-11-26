7611624e30e5ff5f90ed4197133b437ff9e64f64
LOVERAJ <rajdhiman00143@gmail.com>
2024-11-26 19:38:12 +0530

    The Beginning

---
+++ Pixie/Script.js
@@ -0,0 +1,76 @@
+//window means pooore project pe 
+window.addEventListener("wheel",function(dets){
+    if(dets.deltaY>0){
+        gsap.to(".marq",{
+            transform:'translateX(-200%)',
+            repeat:-1,
+            duration:6,
+            ease:'none',
+        })
+        gsap.to(".marq i",{
+            rotate:180,
+        });
+    }
+    else{
+        gsap.to(".marq",{
+            transform:'translateX(0%)',
+            repeat:-1,
+            duration:6,
+            ease:'none',
+        })
+        gsap.to(".marq i",{
+            rotate:0,
+        });
+    }})
+//
+//window means pooore project pe 
+window.addEventListener("wheel",function(dets){
+    if(dets.deltaY>0){
+        gsap.to(".marq2",{
+            transform:'translateX(-200%)',
+            repeat:-1,
+            duration:6,
+            ease:'none',
+        })
+        gsap.to(".marq2 i",{
+            rotate:180,
+        });
+    }
+    else{
+        gsap.to(".marq2",{
+            transform:'translateX(0%)',
+            repeat:-1,
+            duration:6,
+            ease:'none',
+        })
+        gsap.to(".marq2 i",{
+            rotate:0,
+        });
+    }})
+//
+
+//window means pooore project pe 
+window.addEventListener("wheel",function(dets){
+    if(dets.deltaY>0){
+        gsap.to(".marq3",{
+            transform:'translateX(-200%)',
+            repeat:-1,
+            duration:2,
+            ease:'none',
+        })
+        gsap.to(".marq3 i",{
+            rotate:180,
+        });
+    }
+    else{
+        gsap.to(".marq3",{
+            transform:'translateX(0%)',
+            repeat:-1,
+            duration:4,
+            ease:'none',
+        })
+        gsap.to(".marq3 i",{
+            rotate:0,
+        });
+    }})
+//
